9061b7e33b1e1d69db89a311ae7133492886700d
Ghost <67106145+Daveshvats@users.noreply.github.com>
2022-07-04 06:29:50 -0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,66 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Api Website\\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//Api Key\\
+global.APIKeys = {
+	'https://zenzapis.xyz': 'DAC2C16412', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+}
+
+//customize settings\\
+global.owner = ['+91 7404854406']
+global.premium = ['+91 7404854406']
+global.ownernomer = '+91 7404854406'
+global.ownername = 'Ghost'
+global.botname = 'Zero'
+global.footer = '©Ghost.'
+global.ig = 'vatsdavesh@gmai.com'
+global.region = 'India'
+global.sc = 'vatsdavesh@gmail.com'
+global.myweb = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+global.packname = 'zero'
+global.author = '⛓️'
+global.sessionName = 'session'
+global.prefa = ['','!','.','⛓️','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: 'Done ✓',
+    admin: 'This Feature Is Only For Admin!',
+    botAdmin: 'Bot Must Be Admin First!',
+    owner: 'This Feature Is Only For Owner!',
+    group: 'Feature Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This Feature Is Only For Bot',
+    wait: 'Wait A minute...',
+    error: 'Error! Maybe Api Key Is Expired🤔!',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 12,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./ZeroMedia/Zero.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
---
+++ textpro.js
@@ -0,0 +1,92 @@
+<<<<<<< HEAD
+const stonetextmaker = require("stonetextmaker");
+
+//TextPro
+stonetextmaker
+  .textpro("https://textpro.me/create-blackpink-logo-style-online-1001.html", [
+    "Stone",
+  ])
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+//TextPro with 2 text
+stonetextmaker
+  .textpro(
+    "https://textpro.me/create-impressive-glitch-text-effects-online-1027.html",
+    ["Techno", "Stone"]
+  )
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+
+  stonetextmaker
+  .textpro(
+    "https://textpro.me/create-realistic-vintage-style-light-bulb-1000.html",
+    ["Techno", "Stone"]
+  )
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+//Photooxy
+stonetextmaker
+  .photooxy(
+    "https://photooxy.com/logo-and-text-effects/create-a-picture-of-love-message-377.html",
+    ["Stone"]
+  )
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+//Photooxy with 2 text
+stonetextmaker
+  .photooxy(
+    "https://photooxy.com/logo-and-text-effects/make-tik-tok-text-effect-375.html",
+    ["Techno", "Stone"]
+  )
+  .then((data) => console.log(data))
+=======
+const stonetextmaker = require("stonetextmaker");
+
+//TextPro
+stonetextmaker
+  .textpro("https://textpro.me/create-blackpink-logo-style-online-1001.html", [
+    "Stone",
+  ])
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+//TextPro with 2 text
+stonetextmaker
+  .textpro(
+    "https://textpro.me/create-impressive-glitch-text-effects-online-1027.html",
+    ["Techno", "Stone"]
+  )
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+
+  stonetextmaker
+  .textpro(
+    "https://textpro.me/create-realistic-vintage-style-light-bulb-1000.html",
+    ["Techno", "Stone"]
+  )
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+//Photooxy
+stonetextmaker
+  .photooxy(
+    "https://photooxy.com/logo-and-text-effects/create-a-picture-of-love-message-377.html",
+    ["Stone"]
+  )
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+//Photooxy with 2 text
+stonetextmaker
+  .photooxy(
+    "https://photooxy.com/logo-and-text-effects/make-tik-tok-text-effect-375.html",
+    ["Techno", "Stone"]
+  )
+  .then((data) => console.log(data))
+>>>>>>> 0bcaff15ad2fd7027831cb45b12215596385ecf8
+  .catch((err) => console.log(err))
